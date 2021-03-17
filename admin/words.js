d2fc67cdde44b0074d8bf352e9ae93ad1973adcb
deMynchi <80831885+deMynchi@users.noreply.github.com>
2021-03-17 14:39:05 +0100

    Initial Commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'meteoswiss adapter settings': {
+        en: 'Adapter settings for meteoswiss',
+        de: 'Adaptereinstellungen für meteoswiss',
+        ru: 'Настройки адаптера для meteoswiss',
+        pt: 'Configurações do adaptador para meteoswiss',
+        nl: 'Adapterinstellingen voor meteoswiss',
+        fr: "Paramètres d'adaptateur pour meteoswiss",
+        it: "Impostazioni dell'adattatore per meteoswiss",
+        es: 'Ajustes del adaptador para meteoswiss',
+        pl: 'Ustawienia adaptera dla meteoswiss',
+        'zh-cn': 'meteoswiss的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'option1',
+        ru: 'option1',
+        pt: 'option1',
+        nl: 'option1',
+        fr: 'option1',
+        it: 'option1',
+        es: 'option1',
+        pl: 'option1',
+        'zh-cn': 'option1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'option2',
+        ru: 'option2',
+        pt: 'option2',
+        nl: 'option2',
+        fr: 'option2',
+        it: 'option2',
+        es: 'option2',
+        pl: 'option2',
+        'zh-cn': 'option2',
+    },
+};
